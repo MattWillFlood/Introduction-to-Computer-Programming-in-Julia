9ddb7d33c70ae01a72f8e4ad1eab9e4a8487cd97
Matt <mattwillflood@gmail.com>
2022-02-25 01:02:39 +0100

    First update of week3 stuff

---
+++ Week 3/Week3.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.14.7
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6957aa70-95cd-11ec-3fa2-ed38d0d958be
+for x = 1:100
+	println(sum(1:x))
+end
+
+# ╔═╡ Cell order:
+# ╠═6957aa70-95cd-11ec-3fa2-ed38d0d958be
