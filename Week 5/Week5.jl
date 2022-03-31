8207e568afa847a731737d778d561e41ff93481b
Matt <matthew.flood@lih.lu>
2022-03-31 18:44:04 +0200

    Update Week5.jl

--- Week 5/Week5.jl
+++ Week 5/Week5.jl
@@ -51,7 +51,7 @@ readdlm(source, delim::AbstractChar,
 =#
 
 # ╔═╡ 4ffbf08e-cac5-44ad-bc51-af7a66e73216
-Data_Ralphy[:,1]
+Data_Ralphy[1,:]
 
 # ╔═╡ c14d4afc-1473-4e1e-ac37-ef0277877843
 Coco_pops, Coco_Heads = readdlm("Covid_in_Europe.txt", ',', header=true)
@@ -81,23 +81,60 @@ begin
 	Scot_Rain, _ = readdlm("ScotlandRain.txt", header=true, skipstart=5)
 end
 
+# ╔═╡ 802765fe-3f42-47ec-b676-b62527c6addf
+begin
+	Scot_Sun_Take2 = readdlm("ScotlandSun.txt", header=true, skipstart=5)
+end
+
+# ╔═╡ 29cf15e9-a122-4594-812b-2677996b9a9b
+Scot_Sun_Take2[2]
+
+# ╔═╡ 27dfb977-2e6c-43a3-9f56-8684689c6be1
+Scot_Sun
+
 # ╔═╡ a9b4c98e-e89b-4d27-9daa-7f53318f3b1b
+# Here we want to plot the average sunshiine in Scotland in January from the beginning of the record.
+# We are going to overlay the plot for January with the plot for May.
+
 begin
 	plot(Scot_Sun[:,1], Scot_Sun[:,2], label="january", lw=3)
-	plot!(Scot_Sun[1:end-1,1], Scot_Sun[1:end-1,6], label = "June", lw = 3)
+	plot!(Scot_Sun[1:end-1,1], Scot_Sun[1:end-1,6], label = "May", lw = 3)
 end
 
 # ╔═╡ 6794ea21-04be-4897-a646-5c9053305777
+# Here we want to plot multiple lines in our plot corresponding to columns in a matrix/array.
+
+# Here we use the function 'float' to convert the array section into numbers
 plot(Scot_Sun[1:end-1,1], float.(Scot_Sun[1:end-1,2:7]), lw=3)
 
+# ╔═╡ 84cbf844-5a0c-42b5-a592-5cda92d452c8
+g = 7
+
+# ╔═╡ 8fedbbd4-4b76-4202-a2af-000d11286525
+float(g)
+
+# ╔═╡ 62233778-aaf2-43a5-ba79-0dc6df61fae9
+float.(Scot_Rain[1:end-1,[2,3,12,13]])
+
 # ╔═╡ 99e19ffe-4f3f-451f-b70d-b67794ff1790
+# The example demonstrates that we need to use dot notation when applying a function to each value in a collection.
+
 begin
-	Average_Winter_Rain = mean(float.(Scot_Rain[1:end-1,[1,2,11,12]]), dims=2)
-	Average_Summer_Rain = mean(float.(Scot_Rain[1:end-1,5:8]), dims=2)
+	Average_Winter_Rain = mean(float.(Scot_Rain[1:end-1,[2,3,12,13]]), dims=2)
+	Average_Summer_Rain = mean(float.(Scot_Rain[1:end-1,6:9]), dims=2)
 end
+
+# ╔═╡ ee44190f-c43a-49e6-94ca-a4fdc942aab8
+Average_Winter_Rain
 
 # ╔═╡ c828beb9-c8b2-4046-879d-3e6ceb8524fa
 scatter(Average_Winter_Rain, Average_Summer_Rain, marker=:square, hover=Scot_Rain[1:end-1])
+
+# ╔═╡ dffc07da-2cb6-4d66-aeb6-a6bdac0fac8a
+xlabel!("Winter Rain")
+
+# ╔═╡ 5839a26a-ce28-4ba8-a47b-92fa152e1f47
+ylabel!("Summer Rain")
 
 # ╔═╡ 8ccb4838-9335-40a3-85f9-feddd106bb05
 kurtosis(Average_Winter_Rain)
@@ -110,6 +147,9 @@ Random_Data, _ = readdlm("RandomData.txt", header=true)
 
 # ╔═╡ f09f7a94-6016-4ed0-a28d-5dc61cf469e5
 RandomData = float.(Random_Data[:,1:2])
+
+# ╔═╡ 64cb6d33-c332-4225-a58d-860d11567d62
+Random_Data[:,1]
 
 # ╔═╡ 1c51ee9d-6539-4d77-92a3-e746159c7940
 Hist1 = fit(Histogram, RandomData[:,1])
@@ -135,6 +175,8 @@ SetA = Set(rand(1:100,20))
 # ╔═╡ ebb0f63e-54bb-4fac-b5b7-6067b4895d0c
 SetB = Set(rand(1:30,10))
 
+# This is a set of 10 random integers between 1 and 30
+
 # ╔═╡ 77b5793b-7b63-45d4-acdc-c20afc9a57e3
 SetA ∪ SetB
 
@@ -154,6 +196,17 @@ begin
 	marginalkde(x, x+y)
 end
 
+# ╔═╡ ea5139a6-6001-45c0-9cdc-74328bff2029
+begin 
+singers = RDatasets.dataset("lattice", "singer")
+@df singers violin(string.(:VoicePart), :Height, linewidth=0)
+@df singers boxplot!(string.(:VoicePart), :Height, fillalpha=0.75, linewidth=2)
+@df singers dotplot!(string.(:VoicePart), :Height, marker=(:black, stroke(0)))
+end
+
+# ╔═╡ 601fff6b-3e89-4d40-8047-e21860edd861
+NaN()
+
 # ╔═╡ Cell order:
 # ╠═2b77c301-778a-4e7d-b176-194362ebec5e
 # ╠═d58cad3e-a866-4e0e-8f29-821188861a12
@@ -170,14 +223,24 @@ end
 # ╠═b69d4ef0-1a96-4461-b676-a947ff2a6805
 # ╠═f074790a-ffc1-4f06-9068-29f3337313b7
 # ╠═8a294070-022e-4557-8529-cdb513806b79
+# ╠═802765fe-3f42-47ec-b676-b62527c6addf
+# ╠═29cf15e9-a122-4594-812b-2677996b9a9b
+# ╠═27dfb977-2e6c-43a3-9f56-8684689c6be1
 # ╠═a9b4c98e-e89b-4d27-9daa-7f53318f3b1b
 # ╠═6794ea21-04be-4897-a646-5c9053305777
+# ╠═84cbf844-5a0c-42b5-a592-5cda92d452c8
+# ╠═8fedbbd4-4b76-4202-a2af-000d11286525
+# ╠═62233778-aaf2-43a5-ba79-0dc6df61fae9
 # ╠═99e19ffe-4f3f-451f-b70d-b67794ff1790
+# ╠═ee44190f-c43a-49e6-94ca-a4fdc942aab8
 # ╠═c828beb9-c8b2-4046-879d-3e6ceb8524fa
+# ╠═dffc07da-2cb6-4d66-aeb6-a6bdac0fac8a
+# ╠═5839a26a-ce28-4ba8-a47b-92fa152e1f47
 # ╠═8ccb4838-9335-40a3-85f9-feddd106bb05
 # ╠═5d6110b1-a9fd-4a33-b948-ff4d7635262e
 # ╠═dc526d28-126d-43fb-bbab-52583a4065cd
 # ╠═f09f7a94-6016-4ed0-a28d-5dc61cf469e5
+# ╠═64cb6d33-c332-4225-a58d-860d11567d62
 # ╠═1c51ee9d-6539-4d77-92a3-e746159c7940
 # ╠═311a2e58-f62c-4802-94e8-6a1cadd9482b
 # ╠═3a250b90-5b87-4a6a-857d-84ff99dab1d2
@@ -190,3 +253,5 @@ end
 # ╠═c5c386a5-44d9-45c4-80f7-99b77508933c
 # ╠═0ffe5018-e652-40df-8489-b7dee1c06ba8
 # ╠═1f7a5ea5-bea1-40ee-a6c0-e304391893c4
+# ╠═ea5139a6-6001-45c0-9cdc-74328bff2029
+# ╠═601fff6b-3e89-4d40-8047-e21860edd861
